a25eaf0eabb428d7b6bbaa361b7b82ee4e38db71
Flo2503 <bruel.florent@gmail.com>
2020-01-12 21:16:30 +0100

    test

---
+++ Reciplease/SearchRecipesFonctionality/ApiManager.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Reciplease
+//
+//  Created by Flo on 12/01/2020.
+//  Copyright © 2020 Flo. All rights reserved.
+//
+
+import Foundation
+
+class ApiManager {
+    let api = "2f197fb8d0d4d4b720ddd00d89523c43"
+    let appId = "f8a5ab80"
+}
