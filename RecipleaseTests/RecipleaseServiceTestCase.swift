97756c21965e5747289dceaf016ba67ee2d637ea
Flo2503 <bruel.florent@gmail.com>
2020-04-15 01:23:49 +0200

    change some icons to be used on iOS 11 to 13

--- RecipleaseTests/RecipleaseServiceTestCase.swift
+++ RecipleaseTests/RecipleaseServiceTestCase.swift
@@ -10,19 +10,19 @@ import XCTest
 import Alamofire
 import Mockingjay
 @testable import Reciplease
-/*
+
 class RecipleaseServiceTestCase: XCTestCase {
 
     let webService = EdanamWebService()
     let ingredients = ["chicken", "apple", "chocolate"]
-    
+
     func testToto() {
-        
-        let excpected = [Recipes(label: "Eat For Eight Bucks: Crunchy Chicken Salad Sandwiches with Bacon Recipe",
+
+        let excpected = Recipes(label: "Eat For Eight Bucks: Crunchy Chicken Salad Sandwiches with Bacon Recipe",
                                  image: "https://www.edamam.com/web-img/b18/b18aa24c520208fcbc0ddb05861c462a.jpg",
                                  url: "http://www.seriouseats.com/recipes/2011/03/eat-for-eight-bucks-crunchy-chicken-salad-sandwiches-recipe.html",
                                  yield: 2,
-                                 ingredientLines: [ "1 pound boneless, skinless chicken thighs",
+                                 ingredientLines: ["1 pound boneless, skinless chicken thighs",
                                    "2 small carrots, 1 cut into rough chunks, the other diced",
                                    "1 small onion, halved",
                                    "2 medium celery stalks, 1 cut into rough chunks, the other diced",
@@ -33,17 +33,16 @@ class RecipleaseServiceTestCase: XCTestCase {
                                    "4 tablespoons mayonnaise",
                                    "Freshly ground pepper",
                                    "4 slices bakery whole grain bread"],
-                                 totalTime: 60)]
-        
-        let path = Bundle.main.path(forResource: "yourJSONfileName", ofType: "json")
-        var jsonData = try! String(contentsOfFile: path!).data(using: .utf8)!
-        
+                                 totalTime: 60)
+
+        let path = Bundle.main.path(forResource: "FakeRecipe", ofType: "json")
+        let jsonData = try! String(contentsOfFile: path!).data(using: .utf8)!
+
         stub(everything, json(jsonData))
-        
+
         let expectation = XCTestExpectation(description: "wait for queue change")
         webService.getData(for: ingredients, callback: { (success, recipes) in
-            
+            XCTAssertEqual(excpected, json)
         })
     }
 }
-*/
